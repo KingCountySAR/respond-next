3e2d223e16948486a631b0faa401fa9ba8a9c7a4
Valon Miller <valon.miller@gmail.com>
2023-06-03 08:13:36 -0700

    :Abstract Event tiles to an EventTile component

---
+++ src/app/(main)/EventTile.tsx
@@ -0,0 +1,29 @@
+import { Card, CardActionArea, CardActions, CardContent, Typography } from "@mui/material";
+import { Activity } from '@respond/types/activity';
+import { StatusUpdater } from '@respond/components/StatusUpdater';
+import { getActiveParticipants, isActive, getActivityPath } from '@respond/lib/client/store/activities';
+import Link from 'next/link';
+
+export const EventTile = ({ activity }: { activity: Activity }) => {
+
+    return (
+        <Card key={activity.id}>
+            <CardActionArea component={Link} href={`/${getActivityPath(activity)}/${activity.id}`}> 
+            <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+                {activity.idNumber} {activity.title}
+            </Typography>
+            <Typography>
+                Active Responders: {getActiveParticipants(activity).length}
+            </Typography>
+            </CardContent>
+            </CardActionArea>
+            {(isActive(activity)) && (
+                <CardActions>
+                    <StatusUpdater activity={activity} />
+                </CardActions>
+            )}
+        </Card>
+    );
+
+}
