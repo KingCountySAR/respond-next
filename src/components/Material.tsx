8d433174217feffd7d420a5904752ca50e880462
Matt Cosand <matt@cosand.net>
2023-06-04 23:16:37 -0700

    Track miles on change status form

--- src/components/Material.tsx
+++ src/components/Material.tsx
@@ -1,6 +1,25 @@
+// Importing from the inner files is supposed to help compile time / tree shaking.
+
+export { default as Box } from '@mui/material/Box';
+export { default as Button } from '@mui/material/Button';
+export { default as Dialog } from '@mui/material/Dialog';
+export { default as DialogActions } from '@mui/material/DialogActions';
+export { default as DialogContent } from '@mui/material/DialogContent';
+export { default as DialogContentText } from '@mui/material/DialogContentText';
+export { default as DialogTitle } from '@mui/material/DialogTitle';
 export { default as List } from '@mui/material/List';
 export { default as ListItem } from '@mui/material/ListItem';
 export { default as createTheme } from '@mui/material/styles/createTheme';
 export type { ThemeOptions } from '@mui/material/styles/createTheme';
 export { default as ThemeProvider } from '@mui/material/styles/ThemeProvider';
 export { default as useMediaQuery } from '@mui/material/useMediaQuery';
+export { default as Select } from '@mui/material/Select';
+export { default as MenuItem } from '@mui/material/MenuItem'
+export { default as FormControl } from '@mui/material/FormControl';
+export { default as FormControlLabel } from '@mui/material/FormControlLabel';
+export { default as FormHelperText } from '@mui/material/FormHelperText';
+export { default as Radio } from '@mui/material/Radio';
+export { default as RadioGroup } from '@mui/material/RadioGroup';
+export { default as TextField } from '@mui/material/TextField';
+export { default as Typography } from '@mui/material/Typography';
+export { default as Stack } from '@mui/system/Stack';
--- src/types/activity.ts
+++ src/types/activity.ts
@@ -40,6 +40,7 @@ export interface Participant {
   organizationId: string;
   timeline: ResponderUpdate[];
   tags?: string[];
+  miles?: number;
 }
 
 export interface ParticipatingOrg {
