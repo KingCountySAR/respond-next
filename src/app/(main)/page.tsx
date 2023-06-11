3c65539489d9670184ae38ea42caf724183d3734
Valon Miller <valon.miller@gmail.com>
2023-06-10 19:56:35 -0700

    Numerous Improvements to the main UI

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,17 +1,17 @@
 'use client';
 import Link from 'next/link';
 //import { Inter } from 'next/font/google';
-import { Box, Button, Stack, Typography } from "@mui/material";
+import { Box, Button, Stack, Typography, Chip } from "@mui/material";
 
 //import styles from './page.module.css';
-import { useAppDispatch, useAppSelector } from '@respond/lib/client/store';
+import { useAppSelector } from '@respond/lib/client/store';
 import { canCreateEvents, canCreateMissions } from '@respond/lib/client/store/organization';
-import { buildActivityTypeSelector, getActiveParticipants, isActive, isComplete } from '@respond/lib/client/store/activities';
-import { useEffect, useState } from 'react';
+import { buildActivityTypeSelector, buildMyActivitySelector, getActiveParticipants, isActive, isComplete } from '@respond/lib/client/store/activities';
+import { useEffect } from 'react';
 import { Activity } from '@respond/types/activity';
-import { MyActivity } from './MyActivity';
 import addDays from 'date-fns/addDays'
 import { EventTile } from './EventTile';
+import { OrganizationChip } from './OrganizationChip';
 
 //const inter = Inter({ subsets: ['latin'] })
 
@@ -29,6 +29,12 @@ function filterActivitiesForDisplay(activities: Activity[], maxCompletedVisible:
 }
 
 export default function Home() {
+  let myActivities = useAppSelector(buildMyActivitySelector());
+
+  function getMyStatus(activity: Activity) {
+    return myActivities.find(f => f.activity.id === activity.id)?.status.status;
+}
+
   const maxCompletedActivitiesVisible = 3;
   const oldestCompletedActivityVisible = addDays(new Date(), -3).getTime();
 
@@ -48,25 +54,47 @@ export default function Home() {
   return (
     <main>
       <Box sx={{mb:3}}>
-        <MyActivity />
-        <Typography variant="h5">Missions</Typography>
+        <Box sx={{mb:1}}>
+          <Typography variant="h5">My Activity</Typography>
+        </Box>
+        <Stack spacing={1}>
+          {myActivities.map(up => (
+            <EventTile key={up.activity.id} activity={up.activity} status={up.status.status} />
+          ))}
+        </Stack>
+      </Box>
+      <Box sx={{mb:3}}>
+        <Box sx={{mb:1}}>
+          <Typography variant="h5">Missions</Typography>
+        </Box>
         <Stack spacing={1}>
           {missions.map(a => (
-            <EventTile key={a.id} activity={a} />
+            <EventTile key={a.id} activity={a} status={getMyStatus(a)}>
+              <Box>State #: {a.idNumber}</Box>
+              <Box>Active Responders: {getActiveParticipants(a).length}</Box>
+              <Box sx={{ pt: 1 }}>
+                  {Object.entries(a.organizations ?? {}).map(([id, org]) => <OrganizationChip key={id} org={org} />)}
+              </Box>
+            </EventTile>
           ))}
           {missions.length === 0 && <Typography>No recent missions</Typography>}
         </Stack>
         {canCreateM ? ( 
           <Box sx={{mt: 2, display:'flex', flexDirection:'row-reverse'}}> 
             <Button variant="outlined" component={Link} href="/mission/new">New Mission</Button> 
-          </Box> 
+          </Box>
         ) : null} 
       </Box>
       <Box>
-        <Typography variant="h5">Trainings and Other Events</Typography>
+        <Box sx={{mb:1}}>
+          <Typography variant="h5">Trainings and Other Events</Typography>
+        </Box>
         <Stack spacing={1}>
           {events.map(a => (
-            <EventTile key={a.id} activity={a} />
+            <EventTile key={a.id} activity={a}>
+              <Box>State #: {a.idNumber}</Box>
+              <Box>Participants: {getActiveParticipants(a).length}</Box>
+            </EventTile>
           ))}
           {events.length === 0 && <Typography>No recent events</Typography>}
         </Stack>
