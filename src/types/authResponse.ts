8b57e69ba1dd87ad4c80d630b738c334e3a58147
Colin Williams <colin.williams@kc4x4sar.org>
2023-06-30 16:49:54 -0700

    Build fix from login errors PR.

--- src/types/authResponse.ts
+++ src/types/authResponse.ts
@@ -2,7 +2,7 @@ import { MyOrganization } from './organization'
 import { UserInfo } from './userInfo'
 
 export interface AuthResponse {
-    userInfo: UserInfo | undefined
-    organization: MyOrganization | undefined
-    error: string | undefined
+    userInfo?: UserInfo
+    organization?: MyOrganization
+    error?: string
 }
