a818e4b9851a6b61f81b319c70694fe773c9106f
Matt Cosand <mcosand@users.noreply.github.com>
2023-09-27 09:05:53 -0700

    Try different viewport setting to avoid zoomed-in view on navigation (#47)
    The site will currently change viewport zoom while navigating the app. Try a more restrictive viewport setting to try to stop this from happening.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ import "./globals.css"
 export const metadata = {
   title: 'Respond Site',
   description: 'Respond to search and rescue activities',
+  viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
 }
 
 export default async function RootLayout({
