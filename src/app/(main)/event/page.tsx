f904cbefd5d63b940e8aa8c1038bfc8e6c17d6cf
Matt Cosand <matt@cosand.net>
2023-10-03 22:05:25 -0700

    Create pages + APIs to list all missions/events

---
+++ src/app/(main)/event/page.tsx
@@ -0,0 +1,6 @@
+'use client';
+import { ActivityListPage } from '@respond/components/activities/ActivityListPage';
+
+export default function MissionList() {
+  return <ActivityListPage activityType="events" />;
+}
