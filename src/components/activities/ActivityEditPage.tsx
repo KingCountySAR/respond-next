4834810eca16e8afbf849c1c4a8ffcdcfcc441cf
Colin Williams <colin.williams@kc4x4sar.org>
2023-11-29 09:57:39 -0800

    Tweaked grid layout

--- src/components/activities/ActivityEditPage.tsx
+++ src/components/activities/ActivityEditPage.tsx
@@ -285,8 +285,8 @@ export const ActivityEditPage = ({ activityType, activityId }: { activityType: A
             />
           </Grid>
 
-          <Grid item xs={12} spacing={1} sx={{ mt: 2 }}>
-            <Stack direction="column" spacing={1}>
+          <Grid container item xs={12} spacing={1} sx={{ mt: 1 }}>
+            <Grid item xs={12} sm={6}>
               {activityType === 'missions' ? null : (
                 <Grid item xs={12}>
                   <FormGroup>
@@ -302,18 +302,20 @@ export const ActivityEditPage = ({ activityType, activityId }: { activityType: A
               </Grid>
 
               {isNew && (
-                <Grid item xs={12}>
+                <Grid item xs={12} sx={{ my: 1 }}>
                   <Box>{org.title} will start as a participating unit.</Box>
                 </Grid>
               )}
-            </Stack>
+            </Grid>
 
-            <Stack direction="row" justifyContent="flex-end" alignItems="center" spacing={1}>
-              <Button onClick={() => router.back()}>Cancel</Button>
-              <Button type="submit" variant="contained">
-                Save {activityType === 'missions' ? 'Mission' : 'Event'}
-              </Button>
-            </Stack>
+            <Grid item xs={12} sm={6}>
+              <Stack direction="row" justifyContent="flex-end" spacing={1}>
+                <Button onClick={() => router.back()}>Cancel</Button>
+                <Button type="submit" variant="contained">
+                  Save {activityType === 'missions' ? 'Mission' : 'Event'}
+                </Button>
+              </Stack>
+            </Grid>
           </Grid>
         </Grid>
       </form>
