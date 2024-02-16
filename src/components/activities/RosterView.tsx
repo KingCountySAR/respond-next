75912e383683122d20c21f51198f9330b631bb15
Valon Miller <valon.miller@gmail.com>
2024-02-16 07:27:12 -0800

    Roster Print/Save PDF View and CSV Download (#83)
    * roster print view and csv download
    
    * rename id to participant_id in csv export
    
    * implement react-to-print

--- src/components/activities/RosterView.tsx
+++ src/components/activities/RosterView.tsx
@@ -1,101 +1,103 @@
-import { Box, Paper, Stack, Table, TableBody, TableCell, TableHead, TableRow, Typography } from '@mui/material';
-import { differenceInCalendarDays } from 'date-fns';
+import { Box, Button, Divider, Paper, Stack, Table, TableBody, TableCell, TableHead, TableRow, Typography } from '@mui/material';
+import { differenceInCalendarDays, format as formatDate } from 'date-fns';
+import { forwardRef, useRef } from 'react';
+import { useReactToPrint } from 'react-to-print';
 
 import { ToolbarPage } from '@respond/components/ToolbarPage';
 import { useAppSelector } from '@respond/lib/client/store';
 import { buildActivitySelector } from '@respond/lib/client/store/activities';
-import { getOrganizationName, Participant, ParticipantStatus, ParticipantUpdate } from '@respond/types/activity';
+import { Activity, getOrganizationName, Participant, ParticipantStatus, ParticipantUpdate } from '@respond/types/activity';
 
 import { OutputForm, OutputText, OutputTime } from '../OutputForm';
 
 const headerCellStyle = { fontWeight: 700, width: 20 };
 
-export function RosterView({ activityId }: { activityId: string }) {
+export function RosterReview({ activityId }: { activityId: string }) {
   const activity = useAppSelector(buildActivitySelector(activityId));
+  const rosterEntries = getRosterEntries(activity);
 
-  if (!activity) {
-    return <ActivityNotFound />;
-  }
+  const download = () => {
+    const data = rosterEntries.map((e) => {
+      return [e.participantId, e.participantName, e.organizationName, ...Object.values(e.timestamps).map((t) => (t ? formatDate(t, 'MM/dd/yy hh:mm a') : '')), e.miles]
+        .map(String) // convert every value to String
+        .map((v) => v.replaceAll('"', '""')) // escape double quotes
+        .map((v) => `"${v}"`) // quote it
+        .join(','); // comma-separated;
+    });
+    data.unshift(`"participant_id","participant_name","organization_name","sign_in","arrive_base","depart_base","sign_out","miles"`);
+    const blob = new Blob([data.join('\r\n')], { type: 'text/csv;charset=utf-8;' });
+    const url = URL.createObjectURL(blob);
 
-  const rosterEntries: Array<RosterEntry> = [];
-
-  const buildRosterEntries = (participant: Participant) => {
-    const timeline: Array<ParticipantUpdate> = scrubTimeline(participant.timeline);
-    for (let i = timeline.length - 1; i >= 0; i--) {
-      const stage: RosterStage = rosterStages[timeline[i].status] ?? undefined;
-      if (!stage) continue; // The participant status is not relavent to the roster.
-      let rosterEntry = findRosterEntry(participant);
-      if (rosterEntry === undefined) {
-        if (stage !== RosterStage.SignIn) {
-          continue; // Skip orphaned status transitions by requiring a sign in status first.
-        } else {
-          rosterEntry = createRosterEntry(participant, timeline[i].organizationId);
-        }
-      }
-      rosterEntry.timestamps[stage] = timeline[i].time;
-    }
-    // Miles are currently only tracked in aggregate at the participant level. For now, we only
-    // want to append them to the first roster entry for this participant.
-    const firstEntry = rosterEntries.reverse().find((entry) => entry.participantId === participant.id);
-    if (firstEntry) {
-      firstEntry.miles = participant.miles ?? 0;
-    }
+    // Create a link to download it
+    const anchor = document.createElement('a');
+    anchor.href = url;
+    anchor.setAttribute('download', 'roster.csv');
+    anchor.click();
+    anchor.remove();
   };
 
-  const createRosterEntry = (participant: Participant, organizationId: string) => {
-    const org = getOrganizationName(activity, organizationId);
-    const newEntry = buildRosterEntry(participant, org);
-    rosterEntries.unshift(newEntry);
-    return newEntry;
-  };
-
-  const findRosterEntry = (participant: Participant) => {
-    return rosterEntries.find((entry) => entry.participantId === participant.id && !isComplete(entry));
-  };
-
-  Object.values(activity.participants ?? {}).forEach((participant: Participant) => {
-    buildRosterEntries(participant);
+  const printable = useRef(null);
+  const handlePrint = useReactToPrint({
+    content: () => printable.current,
   });
 
   return (
     <ToolbarPage maxWidth="lg">
-      <Paper>
-        <Table size="small">
-          <TableHead>
-            <TableRow>
-              <TableCell colSpan={7}>
-                <OutputForm columns={2}>
-                  <Stack>
-                    <OutputText label="Name" value={activity?.title}></OutputText>
-                    <OutputText label="State #" value={activity?.idNumber}></OutputText>
-                  </Stack>
-                  <Stack>
-                    <OutputTime time={activity.startTime} label="Start Time"></OutputTime>
-                    <OutputTime time={activity.endTime} label="End Time"></OutputTime>
-                  </Stack>
-                </OutputForm>
-              </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell sx={headerCellStyle}>Particpant Name</TableCell>
-              <TableCell sx={headerCellStyle}>Organization</TableCell>
-              <TableCell sx={headerCellStyle}>Sign In</TableCell>
-              <TableCell sx={headerCellStyle}>Arrive Base</TableCell>
-              <TableCell sx={headerCellStyle}>Depart Base</TableCell>
-              <TableCell sx={headerCellStyle}>Sign Out</TableCell>
-              <TableCell sx={headerCellStyle}>Miles</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rosterEntries.map((entry, i) => (
-              <RosterRow key={i} rosterEntry={entry} activityStartTime={activity.startTime} />
-            ))}
-          </TableBody>
-        </Table>
-      </Paper>
+      <Stack direction="row" flex="1 1 auto" spacing={1} divider={<Divider orientation="vertical" flexItem />}>
+        <Box display="flex" flex="1 1 auto" flexDirection="column">
+          <Paper>{activity ? <Roster ref={printable} activity={activity} rosterEntries={rosterEntries} /> : <ActivityNotFound />}</Paper>
+        </Box>
+        <Stack alignItems="stretch" spacing={2}>
+          <Button variant="outlined" onClick={download}>
+            Download (csv)
+          </Button>
+          <Button variant="outlined" onClick={handlePrint}>
+            Print
+          </Button>
+        </Stack>
+      </Stack>
     </ToolbarPage>
   );
 }
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const Roster = forwardRef(function Roster({ activity, rosterEntries }: { activity: Activity; rosterEntries: Array<RosterEntry> }, ref: any) {
+  if (!rosterEntries.length) return <ActivityNotFound />;
+  return (
+    <Table ref={ref} size="small">
+      <TableHead>
+        <TableRow>
+          <TableCell colSpan={7}>
+            <OutputForm columns={2}>
+              <Stack>
+                <OutputText label="Name" value={activity?.title}></OutputText>
+                <OutputText label="State #" value={activity?.idNumber}></OutputText>
+              </Stack>
+              <Stack>
+                <OutputTime time={activity.startTime} label="Start Time"></OutputTime>
+                <OutputTime time={activity.endTime} label="End Time"></OutputTime>
+              </Stack>
+            </OutputForm>
+          </TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell sx={headerCellStyle}>Particpant Name</TableCell>
+          <TableCell sx={headerCellStyle}>Organization</TableCell>
+          <TableCell sx={headerCellStyle}>Sign In</TableCell>
+          <TableCell sx={headerCellStyle}>Arrive Base</TableCell>
+          <TableCell sx={headerCellStyle}>Depart Base</TableCell>
+          <TableCell sx={headerCellStyle}>Sign Out</TableCell>
+          <TableCell sx={headerCellStyle}>Miles</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {rosterEntries.map((entry, i) => (
+          <RosterRow key={i} rosterEntry={entry} activityStartTime={activity.startTime} />
+        ))}
+      </TableBody>
+    </Table>
+  );
+});
 
 function ActivityNotFound() {
   return (
@@ -208,4 +210,50 @@ const buildRosterEntry = (participant: Participant, organizationName: string): R
 
 const isComplete = (entry: RosterEntry): boolean => {
   return !!entry.timestamps[RosterStage.SignOut];
+};
+
+const getRosterEntries = (activity?: Activity) => {
+  if (!activity) return [];
+
+  const rosterEntries: Array<RosterEntry> = [];
+
+  const buildRosterEntries = (participant: Participant) => {
+    const timeline: Array<ParticipantUpdate> = scrubTimeline(participant.timeline);
+    for (let i = timeline.length - 1; i >= 0; i--) {
+      const stage: RosterStage = rosterStages[timeline[i].status] ?? undefined;
+      if (!stage) continue; // The participant status is not relavent to the roster.
+      let rosterEntry = findRosterEntry(participant);
+      if (rosterEntry === undefined) {
+        if (stage !== RosterStage.SignIn) {
+          continue; // Skip orphaned status transitions by requiring a sign in status first.
+        } else {
+          rosterEntry = createRosterEntry(participant, timeline[i].organizationId);
+        }
+      }
+      rosterEntry.timestamps[stage] = timeline[i].time;
+    }
+    // Miles are currently only tracked in aggregate at the participant level. For now, we only
+    // want to append them to the first roster entry for this participant.
+    const firstEntry = rosterEntries.reverse().find((entry) => entry.participantId === participant.id);
+    if (firstEntry) {
+      firstEntry.miles = participant.miles ?? 0;
+    }
+  };
+
+  const createRosterEntry = (participant: Participant, organizationId: string) => {
+    const org = getOrganizationName(activity, organizationId);
+    const newEntry = buildRosterEntry(participant, org);
+    rosterEntries.unshift(newEntry);
+    return newEntry;
+  };
+
+  const findRosterEntry = (participant: Participant) => {
+    return rosterEntries.find((entry) => entry.participantId === participant.id && !isComplete(entry));
+  };
+
+  Object.values(activity.participants ?? {}).forEach((participant: Participant) => {
+    buildRosterEntries(participant);
+  });
+
+  return rosterEntries;
 };
