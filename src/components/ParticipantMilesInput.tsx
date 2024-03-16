8790f9d1eea4825fe3dc82a67b6a18de1ee506ee
Valon Miller <valon.miller@gmail.com>
2024-03-15 23:41:48 -0700

    revert to initial value on undefined

--- src/components/ParticipantMilesInput.tsx
+++ src/components/ParticipantMilesInput.tsx
@@ -13,19 +13,20 @@ type MilesAction = {
 };
 
 const reducer: Reducer<ParticipantMilesState, MilesAction> = (state: ParticipantMilesState, action: MilesAction) => {
+  const hasMiles = action.miles > 0;
   switch (action.type) {
     case 'set_total': {
-      return { ...state, miles: action.miles };
+      return { ...state, miles: hasMiles ? action.miles : state.initialMiles };
     }
     case 'add_miles': {
-      return { ...state, miles: state.initialMiles + action.miles };
+      return { ...state, miles: state.initialMiles + (hasMiles ? action.miles : 0) };
     }
   }
 };
 
 export function ParticipantMileageInput({ currentMiles, onChange }: { currentMiles: number; onChange: (miles: number) => void }) {
   const [state, dispatch] = useReducer<Reducer<ParticipantMilesState, MilesAction>>(reducer, { initialMiles: currentMiles, miles: 0 });
-  const [value, setValue] = useState(currentMiles);
+  const [value, setValue] = useState(0);
   const [isTotalMiles, setIsTotalMiles] = useState<boolean>(true);
 
   useEffect(() => {
@@ -45,7 +46,7 @@ export function ParticipantMileageInput({ currentMiles, onChange }: { currentMil
           <FormControlLabel value="false" control={<Radio size="small" />} label="Add leg" />
         </RadioGroup>
       </FormControl>
-      <TextField value={value} onChange={(event) => setValue(event.target.value ? parseInt(event.target.value) : 0)} type="number" variant="outlined" label={isTotalMiles ? 'Round-Trip Miles' : 'Leg Miles'} />
+      <TextField inputMode="numeric" onChange={(event) => setValue(event.target.value !== undefined ? parseInt(event.target.value) : 0)} type="number" variant="outlined" label={isTotalMiles ? 'Round-Trip Miles' : 'Leg Miles'} />
       <Typography>New round-trip miles: {state.miles}</Typography>
     </Stack>
   );
