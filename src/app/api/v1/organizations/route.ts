fc3ce7844683dd1916ce7dc1d42a42c136e5ddee
Valon Miller <valon.miller@gmail.com>
2025-02-08 21:53:24 -0800

    fix organizations endpoint

--- src/app/api/v1/organizations/route.ts
+++ src/app/api/v1/organizations/route.ts
@@ -1,13 +1,19 @@
 import { NextRequest, NextResponse } from 'next/server';
 
+import { getCookieAuth, userFromAuth } from '@respond/lib/server/auth';
 import * as Mongo from '@respond/lib/server/mongodb';
 
 export async function GET(_request: NextRequest) {
+  const user = userFromAuth(await getCookieAuth());
+  if (user == null) {
+    return NextResponse.json({ status: 'not authenticated' }, { status: 401 });
+  }
+
   const organizations = await Mongo.getOrganizations();
   if (!organizations) {
     return NextResponse.json({ status: 'get organizations failed' }, { status: 500 });
   }
-  console.log(organizations);
+
   return NextResponse.json({
     status: 'ok',
     data: organizations,
